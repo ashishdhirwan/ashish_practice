63d2cd52042476f996a57b33de7a10b1b0f5dafb
ashishdhirwan <dhirwanashish@gmail.com>
2019-06-11 14:27:44 +0530

    asd

--- brigade.js
+++ brigade.js
@@ -2,6 +2,9 @@ const { events, Job, Group } = require('brigadier')
 
 events.on("push", async () => {
 
+
+
+
   let j = new Job("hello-world","dhirwanashish/asd-devops:v1");
   j.privileged = true;
   
@@ -11,41 +14,57 @@ events.on("push", async () => {
   j.tasks = [
     "sleep 10",
     "echo entered",
-    "gcloud info",
     "echo Hello World!",
     "echo now initializing",
-    //"git",
     "git version",
-    "git init",
-    "echo gitiiiiiiiiiiiiiiiiiiiiiing",   
-    "git tag",
+    "echo gitiiiiiiiiiiiiiiiiiiiiiing",
+   // "git init",
+    "dockerd-entrypoint.sh &",
+    "gcloud auth configure-docker",
+    "gcloud config set project my-project-70505",
+    "gcloud auth activate-service-account --key-file=/mydir/vol/my-project-70505-c03a97524e24.json --project=my-project-70505",
+    "echo done-auth",
+    "cd /src",
+    "wget -q -O gitversion https://github.com/screwdriver-cd/gitversion/releases/download/v1.1.1/gitversion_linux_amd64",
+    "chmod u+x ./gitversion",
+    "git fetch --tags -q",
+    "`./gitversion  bump auto && ./gitversion show > pipeline_app_version.txt;,`",
+    "git push --tags >/dev/null 2>&1",
     "latestTag=$(git describe --tags `git rev-list --tags --max-count=1`)",
     "echo $latestTag",
-    //"GIT_COMMIT=git rev-parse HEAD",
-   // "git ",  
-  //  "git rev-parse HEAD",
-    //"echo hello world 2",
-    //"NEEDS_TAG= git describe --contains $GIT_COMMIT",
-    //"echo $NEEDS_TAG",
+    "echo doneeeeeeeeeeeeeeeeeeeeeeeeeee",
+    "docker build -t dhirwanashish/ashish_practice_try_2:latest .",
+    "echo done-build",
+    "docker tag dhirwanashish/ashish_practice_try_2:latest gcr.io/my-project-70505/dhirwanashish/ashish_practice_try_2:$latestTag",
+    "echo done-tagging",
+    "docker push gcr.io/my-project-70505/dhirwanashish/ashish_practice_try_2:$latestTag",
     "ls -lart",
-    //"dockerd-entrypoint.sh &",
-    //"gcloud auth configure-docker",
     "echo $(pwd)",
     "ls -lart",
-    //"helm init",
+    "helm init",
     "helm version",
     "helm ls",
     "echo helm running",
     "echo $(pwd)",
     "echo now auth",
     "ls -lart",
-    //"gcloud config set project my-project-70505",
-    //"gcloud auth activate-service-account --key-file=/mydir/vol/my-project-70505-c03a97524e24.json --project=my-project-70505",
-    "cd /src",
-    //"helm install my-chart/",
-    "echo done-auth",
-
+    "cd my-chart/",
+    //"sed -i 's/version.*/version: '$latestTag'/' Chart.yaml",
+    //'sed -i "s/version.* /version: "$latestTag"/" Chart.yaml',
+    'sed -i "s/tag.*/tag: "$latestTag"/" values.yaml',
+    //`sed -i 's/tag.*/tag: "$latestTag"/' values.yaml`,	
+    "cat values.yaml",
+    "cd ..",
+    "helm ls",
+    "helm upgrade hissing-lobster my-chart/",
+    "echo done-work",
+   // "docker build -t dhirwanashish/ashish_practice_try_1:latest .",
+   // "echo done-build",
+   // "docker tag dhirwanashish/ashish_practice_try_1:latest gcr.io/my-project-70505/dhirwanashish/ashish_practice_try_1:$latestTag",
+   // "echo done-tagging",
+   // "docker push gcr.io/my-project-70505/dhirwanashish/ashish_practice_try_1:$latestTag"
   ]
+  
    j.run();
 });
 
