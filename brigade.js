b75171a84baa9fb9c57b3f47449f4966b2fb2b16
ashishdhirwan <dhirwanashish@gmail.com>
2019-06-12 14:46:10 +0530

    hey

--- brigade.js
+++ brigade.js
@@ -62,8 +62,8 @@ events.on("push", async () => {
     "cat values.yaml",
     "cd ..",
     "helm ls",
-    "helm install my-chart/",
-    //"helm upgrade joyous-goat my-chart/",
+    //"helm install my-chart/",
+    "helm upgrade giggly-rabbit my-chart/",
     "echo done-work",
 
 ]
