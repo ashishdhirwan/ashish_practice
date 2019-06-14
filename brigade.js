809a6b0ad785c4f31c7975f55a479ed5c10123d4
ashishdhirwan <dhirwanashish@gmail.com>
2019-06-14 19:34:42 +0530

    ddone

--- brigade.js
+++ brigade.js
@@ -1,12 +1,12 @@
 const { events, Job } = require("brigadier")
 
 events.on("push", (e) => {
-  var job = new Job("cacher", "alpine:3.4")
-  job.cache.enabled = true
+  var job = new Job("storage", "alpine:3.4")
+  job.storage.enabled = true
 
   job.tasks = [
-    "echo " + e.buildID + " >> /mnt/brigade/cache/jobs.txt",
-    "cat /mnt/brigade/cache/jobs.txt"
+    "echo " + e.buildID + " >> /mnt/brigade/share/jobs.txt",
+    "cat /mnt/brigade/share/jobs.txt"
   ]
 
   job.run()
