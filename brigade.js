3ff56b85f1eb8689bc4440abab3021bc86b21824
ashishdhirwan <dhirwanashish@gmail.com>
2019-06-17 18:32:54 +0530

    das

--- brigade.js
+++ brigade.js
@@ -3,14 +3,14 @@ const { events, Job, Group } = require("brigadier")
 events.on("push", () => {
   var dest = "/mnt/brigade/share/hello.txt";
   var hello = new Job("hello", "alpine:3.4", ["echo hello >" + dest]);
-//  var goodbye = new Job("goodbye", "alpine:3.4", ["cat " + dest])
+  var goodbye = new Job("goodbye", "alpine:3.4", ["cat " + dest])
   hello.storage.enabled = true;
-//  goodbye.storage.enabled = true
+  goodbye.storage.enabled = true
 
   //hello.resourceRequests.memory = "1Gi";
   //hello.resourceRequests.cpu = "500m";
   //hello.resourceLimits.memory = "1Gi";
 
-  hello.run();
-  //Group.runEach([hello, goodbye])
+  //hello.run();
+  Group.runEach([hello, goodbye])
 })
