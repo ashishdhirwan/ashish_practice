4fa038540a569cdc8709499180316c293499c503
ashishdhirwan <dhirwanashish@gmail.com>
2019-06-17 11:23:34 +0530

    testing

--- brigade.js
+++ brigade.js
@@ -2,10 +2,10 @@ const { events, Job, Group } = require("brigadier")
 
 events.on("push", () => {
   var dest = "/mnt/brigade/share/hello.txt"
-  var hello = new Job("hello", "alpine:3.4", ["echo hello" > +dest])
-  var goodbye = new Job("goodbye", "alpine:3.4", ["cat " + dest])
+  var hello = new Job("hello", "alpine:3.4", ["echo hello" > + dest])
+ // var goodbye = new Job("goodbye", "alpine:3.4", ["cat " + dest])
   hello.storage.enabled = true
-  goodbye.storage.enabled = true
-  
-  Group.runEach([hello, goodbye])
+ // goodbye.storage.enabled = true
+  run.hello()
+  //Group.runEach([hello, goodbye])
 })
