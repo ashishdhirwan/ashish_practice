147799321a8958817d88e34a17cfd7126f7f26e8
ashishdhirwan <dhirwanashish@gmail.com>
2019-06-17 16:46:51 +0530

    done

--- brigade.js
+++ brigade.js
@@ -1,7 +1,7 @@
 const { events, Job, Group } = require("brigadier")
 
 events.on("push", () => {
-  var dest = "/volumes/hello.txt";
+  var dest = "/mnt/brigade/share/hello.txt";
   var hello = new Job("hello", "alpine:3.4", ["echo hello" > + dest]);
   var goodbye = new Job("goodbye", "alpine:3.4", ["cat " + dest])
   hello.storage.enabled = true;
