7120122e82d33d7aa36ccedb5208fbf7b6a6afcc
ashishdhirwan <dhirwanashish@gmail.com>
2019-07-01 16:45:37 +0530

    test

--- brigade.js
+++ brigade.js
@@ -8,7 +8,7 @@ events.on("push", async (e, project) => {
   console.log("Received a push event");
   var dest = "/mnt/brigade/share/keys.txt";
 
-/*   let linttask = new Job("linttask","node:slim");
+  let linttask = new Job("linttask","node:slim");
   linttask.storage.enabled = true;
   linttask.tasks = [
   "cd src/",
@@ -20,15 +20,15 @@ events.on("push", async (e, project) => {
   //  devtask.git_tag_store(dest),
   //  "echo storagedone"
   ];
- */
-   let gittask = new Job("gittask","dhirwanashish/asd-devops:v1");
+ 
+/*    let gittask = new Job("gittask","dhirwanashish/asd-devops:v1");
   gittask.storage.enabled = true;
   gittask.tasks = [
   //"cd src/",
   ...devtask.git_auth(),
   ...devtask.git_versioniong(),
   ...devtask.git_tag_store(dest)
-  ];
+  ]; */
 
 
 /* 
